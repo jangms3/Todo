6b1b16ce107fdb90abd27bab77bd0bcb557810f7
jangms3 <github.com/jangms3>
2024-10-17 03:32:59 +0900

    UserDTO

--- src/main/java/com/sparta/todo/dto/UserResponseDto.java
+++ src/main/java/com/sparta/todo/dto/UserResponseDto.java
@@ -1,6 +1,15 @@
 package com.sparta.todo.dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserResponseDto {
     private Long id;
     private String username;
