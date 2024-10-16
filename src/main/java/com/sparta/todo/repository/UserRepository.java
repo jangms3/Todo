a0a55d195803c0f4985304aca7c32537df072fd1
jangms3 <github.com/jangms3>
2024-10-17 03:33:06 +0900

    User Repository

--- src/main/java/com/sparta/todo/repository/UserRepository.java
+++ src/main/java/com/sparta/todo/repository/UserRepository.java
@@ -4,7 +4,4 @@ import com.sparta.todo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByUsername(String username);
-    User findByEmail(String email);
 }
