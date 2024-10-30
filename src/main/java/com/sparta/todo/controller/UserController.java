c405915f49eeb74bf5b3d243dfa83ae0fc814669
jangms3 <github.com/jangms3>
2024-10-30 16:53:18 +0900

    feat: RequestDto 에 Validation 추가하여 API 요청 시 기본적인 사용자 입력검증 기능 추가
    - RequestDto 필드에 Validation 어노테이션 적용
    - API 요청 시 기본적인 사용자 입력을 검증하여 데이터 무결성 및 오류 처리를개선

--- src/main/java/com/sparta/todo/controller/UserController.java
+++ src/main/java/com/sparta/todo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.sparta.todo.controller;
 import com.sparta.todo.dto.UserRequestDto;
 import com.sparta.todo.dto.UserResponseDto;
 import com.sparta.todo.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,7 +30,10 @@ public class UserController {
     }
     // 유저 생성
     @PostMapping
-    public UserResponseDto createUser(@RequestBody UserRequestDto userRequestDto) {
+    public UserResponseDto createUser(@RequestBody @Valid UserRequestDto userRequestDto) {
+        // 유저가 프론트에서 입력을 한다.
+        //1. 처음 들어오는 데이터 requestDto
+        //2. 이 api가 실행되기 전부터 user의 입력이 타당한지 검증 가능.
         return userService.createUser(userRequestDto);
     }
     // 유저 수정
--- src/main/java/com/sparta/todo/dto/UserRequestDto.java
+++ src/main/java/com/sparta/todo/dto/UserRequestDto.java
@@ -1,5 +1,7 @@
 package com.sparta.todo.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,6 +9,8 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class UserRequestDto {
+    @NotNull
     private String username;
+    @Email(message = "이메일 형식을 확인해 주세요.")
     private String email;
 }
